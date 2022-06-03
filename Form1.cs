969c533c52f3ffd6d5c39719aeb4111ef2b00e66
Zabala22 <zabalaemmanuel32@gmail.com>
2022-06-03 13:11:24 +0800

    First commit

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace Tictactoe
+{
+    public partial class Tictactoe : Form
+    {
+        public Tictactoe()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
